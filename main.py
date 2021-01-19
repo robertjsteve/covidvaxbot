b5d2e2e31f1e34d70d492e848f3f5ecf621feb4b
robertjsteve <robertjsteve@gmail.com>
2021-01-18 19:42:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-api.update_status("Up and running!")
 
 def init(shoprite, middlesex, morris, burlington):
     
